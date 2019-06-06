54b21bfb813dd9ed808fdb985730b4346df433c2
jet76 <joe.turner@live.com>
2019-06-06 09:39:58 -0600

    work on counter.c

--- Assignment 1/counter.c
+++ Assignment 1/counter.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <assert.h>
+#include <stdlib.h>
 
-int main() {
-    
+int main(int argc, char *argv[]) {
+    assert(printf("Enter a number: ") > 0);
+    int n;
+    assert(scanf("%d", &n) > 0);
+    int child = getpid(0);
+    assert(printf("Child PID: %d\n", child) > 0);
+    int parent = getppid(0);;
+    assert(printf("Parent PID: %d\n", parent)> 0);
+    for(int i = 1; i <= n; i++){
+        child = getpid(n);
+        assert(printf("Process: %d %d\n", child, i) > 0);
+    }
+    return 0;
 }
