58164d64f4be6072f10fb379d29a2691472d4d64
jet76 <joe.turner@live.com>
2019-06-06 19:44:23 -0600

    update counter.c

--- Assignment 1/counter.c
+++ Assignment 1/counter.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-    assert(printf("Enter a number: ") > 0);
-    int n;
-    assert(scanf("%d", &n) > 0);
-    int child = getpid(0);
+    int child = getpid();
     assert(printf("Child PID: %d\n", child) > 0);
-    int parent = getppid(0);;
-    assert(printf("Parent PID: %d\n", parent)> 0);
-    for(int i = 1; i <= n; i++){
-        child = getpid(n);
-        assert(printf("Process: %d %d\n", child, i) > 0);
+    int parent = getppid();
+    assert(printf("Parent PID: %d\n", parent) > 0);
+    long n = 0;
+    if(argc > 0){
+        char *endptr;
+        n = strtol(argv[1], &endptr, 0);
+        for(int i = 1; i <= n; i++){
+            child = getpid();
+            assert(printf("Process: %d %d\n", child, i) > 0);
+        }
     }
-    return 0;
+    return n;
 }
