02d25917a00b6fe9d90e49054423fbb4d75f8fba
jet76 <joe.turner@live.com>
2019-06-07 17:07:49 -0600

    update counter.c and main.c

--- Assignment 1/counter.c
+++ Assignment 1/counter.c
@@ -3,18 +3,26 @@
 #include <stdlib.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
     int child = getpid();
+    if(child == -1){
+        perror("getpid error");
+        exit(errno);
+    }
     assert(printf("Child PID: %d\n", child) > 0);
     int parent = getppid();
+    if(parent == -1){
+        perror("getppid error");
+        exit(errno);
+    }
     assert(printf("Parent PID: %d\n", parent) > 0);
     long n = 0;
     if(argc > 0){
         char *endptr;
-        n = strtol(argv[1], &endptr, 0);
+        n = strtol(argv[0], &endptr, 0);
         for(int i = 1; i <= n; i++){
-            child = getpid();
             assert(printf("Process: %d %d\n", child, i) > 0);
         }
     }
--- Assignment 1/main.c
+++ Assignment 1/main.c
@@ -1,6 +1,34 @@
+/* Help from Conor McCandless */
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main() {
-    
+    int status;
+    pid_t wait;
+    pid_t f = fork();
+    if(f == -1){
+        perror("fork error");
+        exit(errno);
+    }
+    else if(f == 0){
+        int n = execl("counter", "5", NULL);
+        if(n == -1){
+            perror("execl error");
+            exit(errno);
+        }
+    }
+    else{
+        wait = waitpid(f, &status, 0);
+        if(wait == -1){
+            perror("waitpid error");
+            exit(errno);
+        }  
+        assert(printf("Process %d exited with status: %d\n", f, WEXITSTATUS(status)) > 0); 
+    }
+    return 0;
 }
