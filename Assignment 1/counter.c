40129eea65611f627754849115a4324380e4fe47
jet76 <joe.turner@live.com>
2019-06-10 17:52:19 -0600

    update counter.c

--- Assignment 1/counter.c
+++ Assignment 1/counter.c
@@ -6,24 +6,14 @@
 #include <errno.h>
 
 int main(int argc, char *argv[]) {
-    int child = getpid();
-    if(child < 0){
-        perror("getpid error");
-        exit(errno);
-    }
-    assert(printf("Child PID: %d\n", child) != 0);
-    int parent = getppid();
-    if(parent < 0){
-        perror("getppid error");
-        exit(errno);
-    }
-    assert(printf("Parent PID: %d\n", parent) != 0);
+    assert(printf("Child PID: %d\n", getpid()) != 0);
+    assert(printf("Parent PID: %d\n", getppid()) != 0);
     long n = 0;
     if(argc > 1){
         char *endptr;
         n = strtol(argv[1], &endptr, 0);
         for(int i = 1; i <= n; i++){
-            assert(printf("Process: %d %d\n", child, i) != 0);
+            assert(printf("Process: %d %d\n", getpid(), i) != 0);
         }
     }
     return n;
