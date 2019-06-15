6c46cf7234a634851dcf73b991692950154f5fd8
jet76 <joe.turner@live.com>
2019-06-15 10:47:59 -0600

    Initial implementation of main.c and child.c

---
+++ Assignment 2/child.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    assert(kill(getppid(), SIGURG) >= 0);
+    assert(kill(getppid(), SIGUSR1) >= 0);
+    assert(kill(getppid(), SIGUSR2) >= 0);
+    return 0;
+}
---
+++ Assignment 2/main.c
@@ -0,0 +1,56 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <assert.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+void handler(int sig){
+    switch(sig){
+        case SIGURG:
+            assert(printf("Received SIGURG (%d)\n", sig) != 0);
+            break;
+        case SIGUSR1:
+            assert(printf("Received SIGUSR1 (%d)\n", sig) != 0);
+            break;
+        case SIGUSR2:
+            assert(printf("Received SIGUSR2 (%d)\n", sig) != 0);
+            break;
+    }
+}
+
+int main(){
+
+    struct sigaction action;
+    action.sa_handler = handler;
+    sigemptyset(&action.sa_mask);
+    action.sa_flags = SA_RESTART;
+    assert(sigaction(SIGUSR1, &action, NULL) == 0);
+    assert(sigaction(SIGUSR2, &action, NULL) == 0);
+    assert(sigaction(SIGURG, &action, NULL) == 0);
+
+    int status;
+    pid_t wait;
+    pid_t f = fork();
+    if(f == -1){
+        perror("fork error");
+        exit(errno);
+    }
+    else if(f == 0){
+        int n = execl("child", "cnild", NULL);
+        if(n == -1){
+            perror("execl error");
+            exit(errno);
+        }
+    }
+    else{
+        assert(wait = waitpid(f, &status, 0) >= 0);
+        if(WIFEXITED(status)){
+            assert(printf("Process %d exited with status: %d\n", f, WEXITSTATUS(status)) != 0); 
+        }
+    }
+    return 0;
+}
